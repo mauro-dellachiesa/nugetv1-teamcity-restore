07157162dfb669a1670a454d005610cfd9847d06
Mauro <mauro-dellachiesa@users.noreply.github.com>
2017-09-14 09:34:16 -0300

    Update restore.bat
    Always download the latest available nuget.exe

--- restore.bat
+++ restore.bat
@@ -1,7 +1,7 @@
 ECHO OFF
 IF "%1"=="" GOTO USAGE
 ECHO Downloading nuget.exe
-powershell -command "& { iwr https://dist.nuget.org/win-x86-commandline/v2.8.6/nuget.exe -OutFile nuget.exe }"
+powershell -command "& { iwr https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe }"
 ECHO Restoring local solution nuget packages using %1
 nuGet.exe restore -Source http://%1/guestAuth/app/nuget/v1/FeedService.svc -Source https://www.nuget.org/api/v2/curated-feeds/microsoftdotnet/
 ECHO Deleting nuget.exe
